931d825d592c2bf3270d155094348148aa8f3155
tikhonshw <tikhonshw@gmail.com>
2024-05-13 19:20:49 +0500

    update

--- Less08 Базы данных. C++ и SQL/Практика/8.3/8.3.cpp
+++ Less08 Базы данных. C++ и SQL/Практика/8.3/8.3.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <clocale>
 
-#include "/home/user/###MyDoc/###ItProger/C++/mysql-connector/include/jdbc/mysql_connection.h"
-#include </home/user/###MyDoc/###ItProger/C++/mysql-connector/include/jdbc/cppconn/exception.h>
-#include </home/user/###MyDoc/###ItProger/C++/mysql-connector/include/jdbc/cppconn/prepared_statement.h>
+#include "/home/user/MySoft/itProger/DrvMySQL/mysql/include/jdbc//mysql_connection.h"
+#include </home/user/MySoft/itProger/DrvMySQL/mysql/include/jdbc/cppconn/driver.h>
+#include </home/user/MySoft/itProger/DrvMySQL/mysql/include/jdbc/cppconn/exception.h>
+#include </home/user/MySoft/itProger/DrvMySQL/mysql/include/jdbc/cppconn/prepared_statement.h>
 
+using std::string;
+using std::cout;
+using std::endl;
+
+const string SERVER = "tcp://10.152.36.73:3306";
+const string USER = "root";
+const string PASSWORD = "3333";
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
+    sql::Driver* driver;
+    sql::Connection* conn = nullptr;
+    sql::Statement* stmt;
+
+    try {
+        driver = get_driver_instance();
+        conn = driver->connect(SERVER, USER, PASSWORD);
+    }
+    catch(sql::SQLException& ex) {
+        if (conn != nullptr && !conn->isClosed())
+            conn->close();
+        cout << "Error connection. " << ex.what();
+        system("pause");
+    }
     
+
+
+
     return 0;
 }
