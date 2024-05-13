2dcfa26de849502dcd0fff6788a3f1b2f08d2bb0
tikhonshw <tikhonshw@gmail.com>
2024-05-13 18:55:00 +0500

    Проба запуска MySQL

---
+++ Less08 Базы данных. C++ и SQL/Практика/8.3/8.3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <clocale>
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    
+    return 0;
+}
