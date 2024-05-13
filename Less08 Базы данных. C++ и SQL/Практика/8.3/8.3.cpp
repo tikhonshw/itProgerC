ddc1ad8b86f1b0e3f52fdcae29c61150ff6c3c12
tikhonshw <tikhonsh@gmail.com>
2024-05-13 19:00:40 +0500

    test Mysql

--- Less08 Базы данных. C++ и SQL/Практика/8.3/8.3.cpp
+++ Less08 Базы данных. C++ и SQL/Практика/8.3/8.3.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <clocale>
 
-#include "mysql_connection.h"
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/prepared_statement.h>
+#include "/home/user/###MyDoc/###ItProger/C++/mysql-connector/include/jdbc/mysql_connection.h"
+#include </home/user/###MyDoc/###ItProger/C++/mysql-connector/include/jdbc/cppconn/exception.h>
+#include </home/user/###MyDoc/###ItProger/C++/mysql-connector/include/jdbc/cppconn/prepared_statement.h>
 
 
 int main() {
